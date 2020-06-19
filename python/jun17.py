f9abbfcb17a7dd336f968cb9efbc1f1ab3463bf0
Jessica Scortecci <jessica.scortecci@gmail.com>
2020-06-18 17:49:31 -0700

    adding print examples

--- python/jun17.py
+++ python/jun17.py
@@ -14,3 +14,14 @@ try:
         print("The value of random_number is:", random_number)
 except:
     print('This is not a number')
+
+
+# Print examples
+lista = ['burguer', 'suco', 'pizza', 'pudim']
+lista.insert(0, 123)
+
+print(f'My item {lista[0]}')
+print('My item {}'.format(lista[0]))
+print('My item %s' %(lista[0]))
+print('My item', lista[0])
+print('My item ' + str(lista[0]))      #old version
