7c170e2f2e01bd2f91c98ec14b80714e05fb8967
Bruno Verussa <b.verussa@gmail.com>
2020-06-29 20:30:31 -0700

    Jun 29

--- python/flask_test/hello-flask.py
+++ python/flask_test/hello-flask.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/user/<username>')
+def show_user_profile(username=None):
+    return render_template('user_profile.html', username=username)
